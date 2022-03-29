85cce9a5a4cd7e3a8f1ce525fa51877c238cc6f0
vandrw <v.andrei1455@gmail.com>
2022-03-29 12:11:02 +0200

    added CCT to repo, testing pretrain

---
+++ Compact-Transformers/pretrained.py
@@ -0,0 +1,4 @@
+from src import cct_7_3x1_32
+
+model = cct_7_3x1_32(pretrained=True, progress=True)
+
---
+++ data/get_data.sh
@@ -0,0 +1,5 @@
+wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
+
+tar -xvf cifar-100-python.tar.gz
+
+rm cifar-100-python.tar.gz*
