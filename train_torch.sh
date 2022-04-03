942d521760fbea3a8177207cf064875365c71237
vandrw <v.andrei1455@gmail.com>
2022-04-04 00:15:17 +0200

    fixed paths

--- train_torch.sh
+++ train_torch.sh
@@ -12,7 +12,7 @@ module load PyTorch/1.10.0-fosscuda-2020b jbigkit
 
 source /data/$USER/.envs/deep_torch/bin/activate
 
-cd Deep_Learning_Lab2/Compact-Transformers
+cd Compact-Transformers
 
 ./dist_train.sh 1 -c configs/tiny_imagenet.yml --model cct_14_7x2_224 --epochs 100 --log-wandb /data/$USER/deepl_data/tiny-imagenet-200/train/
 
